5bb797eb4dcea6a3a9bc28758c9939f002fac08f
hyungyu Kim <ypsophila97@icloud.com>
2021-01-23 19:09:22 +0900

    Add swift file for UserListVC, PhotoCollectionVC

---
+++ unsplashTutorial/PhotoCollectionVC.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionVC.swift
+//  unsplashTutorial
+//
+//  Created by kimhyungyu on 2021/01/23.
+//
+
+import UIKit
+
+class PhotoCollection: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("PhotoCollectionVC - viewDidLoad() called")
+    }
+}
---
+++ unsplashTutorial/UserListVC.swift
@@ -0,0 +1,17 @@
+//
+//  UserListVC.swift
+//  unsplashTutorial
+//
+//  Created by kimhyungyu on 2021/01/23.
+//
+
+
+import UIKit
+
+class UserListVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("UserListVC - viewDIdLoad() called")
+    }
+}
