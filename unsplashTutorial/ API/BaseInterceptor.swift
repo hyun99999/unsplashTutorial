9a85901c4cac5ec8ad98f98acf18494f038555e4
hyungyu Kim <ypsophila97@icloud.com>
2021-01-26 15:58:34 +0900

    Add alamofireManager.swift

--- unsplashTutorial/ API/BaseInterceptor.swift
+++ unsplashTutorial/ API/BaseInterceptor.swift
@@ -9,10 +9,20 @@ import Foundation
 import Alamofire
 
 class BaseInterceptor: RequestInterceptor {
+    //
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-        <#code#>
+        print("BaseInterceptor - adapt() called")
+        //헤더추가
+        var request = urlRequest
+        request.addValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type" )
+        request.addValue("application/json; charset=UTF-8", forHTTPHeaderField:"Accept")
+        
+        completion(.success(urlRequest))
     }
+    
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
-        <#code#>
+        //응답에 대한 결과로 정상적인 작업이 되지 않는다면 여기서 설정 가능.
+        print("BseInterceptor - retry() called")
+        completion(.doNotRetry)
     }
 }
--- unsplashTutorial/ API/MyAlamofireManager.swift
+++ unsplashTutorial/ API/MyAlamofireManager.swift
@@ -13,11 +13,16 @@ final class MyAlamofireManager {
     static let shared = MyAlamofireManager()
     
     //인터셉터 : api 호출 시 가로채서 중간에 공통 파라미터를 넣거나 토큰 인증을 하거나 등
-//     let intercepters = Interceptor()
+    let intercepters = Interceptor(interceptors: [
+    BaseInterceptor()
+    ])
     
     // 쿠기 설정
 //    let monitors =
     
     //세션 설정
-    var session = Session.default
+    var session : Session
+    private init() {
+        session = Session(interceptor: intercepters )
+    }
 }
